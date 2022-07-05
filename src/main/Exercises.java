2de245c4713051643efe287d0ba76c2f3fd25129
Brice Ernst <brice.ernst1@gmail.com>
2022-07-05 11:00:45 -0500

    exercises 12-13

--- src/main/Exercises.java
+++ src/main/Exercises.java
@@ -1,8 +1,5 @@
 package main;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-
 public class Exercises {
     public static void main(String[] args) {
         // Exercise 1
@@ -75,6 +72,14 @@ public class Exercises {
         System.out.println(isCatPlaying(true, 10)); //should return false since temperature is not in range 25 - 45
         System.out.println(isCatPlaying(false, 36)); //should return false since temperature is not in range 25 - 35 (summer parameter is false)
         System.out.println(isCatPlaying(false, 35)); //should return true since temperature is in range 25 - 35
+
+        // Exercise 13
+        System.out.println(getDaysInMonth(1, 2020)); //→ should return 31 since January has 31 days.
+        System.out.println(getDaysInMonth(2, 2020)); //→ should return 29 since February has 29 days in a leap year and 2020 is a leap year.
+        System.out.println(getDaysInMonth(2, 2018)); //→ should return 28 since February has 28 days if it's not a leap year and 2018 is not a leap year.
+        System.out.println(getDaysInMonth(-1, 2020)); //→ should return -1 since the parameter month is invalid.
+        System.out.println(getDaysInMonth(1, -2020)); //→ should return -1 since the parameter year is outside the range of 1 to 9999.
+
     }
 
     // Exercise 1
@@ -82,14 +87,14 @@ public class Exercises {
         if (kilometersPerHour < 0){
             return -1;
         }
-        return (long) (Math.round(kilometersPerHour / 1.609));
+        return (Math.round(kilometersPerHour / 1.609));
     }
 
     public static void printConversion(double kilometersPerHour){
         if (kilometersPerHour < 0){
             System.out.println("Invalid Value");
         } else {
-            long milesPerHour = (long) (Math.round(kilometersPerHour / 1.609));
+            long milesPerHour = (Math.round(kilometersPerHour / 1.609));
             System.out.println(kilometersPerHour + " km/h = " + milesPerHour + " mi/h");
         }
     }
@@ -120,11 +125,7 @@ public class Exercises {
         }
         if (year % 4 == 0) {
             if (year % 100 == 0){
-                if (year % 400 == 0){
-                    return true;
-                } else {
-                    return false;
-                }
+                return year % 400 == 0;
             } else {
                 return true;
             }
@@ -214,6 +215,81 @@ public class Exercises {
             return temperature >= 25 && temperature <= 45;
         } else {
             return temperature >= 25 && temperature <= 35;
+        }
+    }
+
+    // Exercise 12
+    public static void printNumberInWord(int number){
+        switch (number){
+            case 0:
+                System.out.println("ZERO");
+                break;
+
+            case 1:
+                System.out.println("ONE");
+                break;
+
+            case 2:
+                System.out.println("TWO");
+                break;
+
+            case 3:
+                System.out.println("THREE");
+                break;
+
+            case 4:
+                System.out.println("FOUR");
+                break;
+
+            case 5:
+                System.out.println("FIVE");
+                break;
+
+            case 6:
+                System.out.println("SIX");
+                break;
+
+            case 7:
+                System.out.println("SEVEN");
+                break;
+
+            case 8:
+                System.out.println("EIGHT");
+                break;
+
+            case 9:
+                System.out.println("NINE");
+                break;
+
+            default:
+                System.out.println("OTHER");
+        }
+    }
+
+    // Exercise 13
+    public static int getDaysInMonth(int month, int year){
+        if (month < 1 || month > 12){
+            return -1;
+        }
+        if (year < 1 || year > 9999){
+            return -1;
+        }
+        switch (month){
+            case 1: case 3: case 5: case 7: case 8: case 10: case 12:
+                return 31;
+
+            case 4: case 6: case 9: case 11:
+                return 30;
+
+            case 2:
+                if (isLeapYear(year)){
+                    return 29;
+                } else {
+                    return 28;
+                }
+
+            default:
+                return -1;
         }
     }
 }
