13de2bafe73cf5fc7bbf261f6e8ba16a48144741
Brice Ernst <brice.ernst1@gmail.com>
2022-07-07 23:05:34 -0500

    exercise 14

--- src/main/Exercises.java
+++ src/main/Exercises.java
@@ -80,6 +80,14 @@ public class Exercises {
         System.out.println(getDaysInMonth(-1, 2020)); //→ should return -1 since the parameter month is invalid.
         System.out.println(getDaysInMonth(1, -2020)); //→ should return -1 since the parameter year is outside the range of 1 to 9999.
 
+        // Exercise 14
+        System.out.println(sumOdd(1, 100)); //→ should return 2500
+        System.out.println(sumOdd(-1, 100)); //→  should return -1
+        System.out.println(sumOdd(100, 100)); //→ should return 0
+        System.out.println(sumOdd(13, 13)); //→ should return 13 (This set contains one number, 13, and it is odd)
+        System.out.println(sumOdd(100, -100)); //→ should return -1
+        System.out.println(sumOdd(100, 1000)); //→ should return 247500
+
     }
 
     // Exercise 1
@@ -291,5 +299,26 @@ public class Exercises {
             default:
                 return -1;
         }
+    }
+
+    // Exercise 14
+    public static boolean isOdd(int number){
+        if (number <= 0){
+            return false;
+        }
+        return (number % 2 != 0);
+    }
+
+    public static int sumOdd(int start, int end){
+        int sum = 0;
+        if (start > end || start < 0){
+            return -1;
+        }
+        for (int i = start; i <= end; i++) {
+            if (isOdd(i)){
+                sum += i;
+            }
+        }
+        return sum;
     }
 }
