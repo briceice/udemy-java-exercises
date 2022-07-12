c3df9e8470d8be442081c3ba26ac87291c33c1ab
Brice Ernst <brice.ernst1@gmail.com>
2022-07-12 15:56:18 -0500

    exercises 27-28

--- src/main/Exercises.java
+++ src/main/Exercises.java
@@ -1,5 +1,7 @@
 package main;
 
+import java.util.Scanner;
+
 public class Exercises {
     public static void main(String[] args) {
         // Exercise 1
@@ -172,6 +174,23 @@ public class Exercises {
         printSquareStar(5);
         printSquareStar(8);
         printSquareStar(2);
+
+        // Exercise 27
+        //inputThenPrintSumAndAverage();
+
+        // Exercise 28
+        System.out.println(getBucketCount(-3.4, 2.1, 1.5, 2)); //→ should return -1 since the width parameter is invalid
+        System.out.println(getBucketCount(3.4, 2.1, 1.5, 2)); //→ should return 3 since the wall area is 7.14, a single bucket can cover an area of 1.5 and Bob has 2 extra buckets home.
+        System.out.println(getBucketCount(2.75, 3.25, 2.5, 1)); //→ should return 3 since the wall area is 8.9375, a single bucket can cover an area of 2.5 and Bob has 1 extra bucket at home.
+
+        System.out.println(getBucketCount(-3.4, 2.1, 1.5)); //→ should return -1 since the width parameter is invalid
+        System.out.println(getBucketCount(3.4, 2.1, 1.5)); //→ should return 5 since the wall area is 7.14, and a single bucket can cover an area of 1.5.
+        System.out.println(getBucketCount(7.25, 4.3, 2.35)); //→ should return 14 since the wall area is 31.175, and a single bucket can cover an area of 2.35.
+
+        System.out.println(getBucketCount(3.4, 1.5)); //→ should return 3 since the area is 3.4 and a single bucket can cover an area of 1.5
+        System.out.println(getBucketCount(6.26, 2.2)); //→ should return 3 since the wall area is 6.26 and a single bucket can cover an area of 2.2.
+        System.out.println(getBucketCount(3.26, 0.75)); //→ should return 5 since the wall area is 3.26, and a single bucket can cover an area of 0.75 .
+
     }
 
     // Exercise 1
@@ -658,5 +677,59 @@ public class Exercises {
                 System.out.println();
             }
         }
+    }
+
+    // Exercise 27
+    public static void inputThenPrintSumAndAverage(){
+        Scanner scanner = new Scanner(System.in);
+        int sum = 0;
+        int count = 0;
+        long average;
+
+        System.out.println("Enter a number: ");
+        while (scanner.hasNextInt()){
+            sum += scanner.nextInt();
+            count ++;
+        }
+
+        if (count == 0){
+            System.out.println("SUM = 0 AVG = 0");
+        } else {
+            average = Math.round((double) sum / count);
+            System.out.println("SUM = " + sum + " AVG = " + average);
+        }
+
+        scanner.close();
+
+    }
+
+    // Exercise 28
+    public static int getBucketCount(double width, double height, double areaPerBucket, int extraBuckets){
+        if (width <= 0 || height <= 0 || areaPerBucket <= 0 || extraBuckets < 0){
+            return -1;
+        }
+
+        double area = width * height;
+        int buckets = (int) Math.ceil(area / areaPerBucket);
+
+        return buckets - extraBuckets;
+    }
+
+    public static int getBucketCount(double width, double height, double areaPerBucket){
+        if (width <= 0 || height <= 0 || areaPerBucket <= 0){
+            return -1;
+        }
+
+        double area = width * height;
+
+        return (int) Math.ceil(area / areaPerBucket);
+    }
+
+    public static int getBucketCount(double area, double areaPerBucket){
+        if (area <= 0 || areaPerBucket <= 0){
+            return -1;
+        }
+
+        return (int) Math.ceil(area / areaPerBucket);
     }
 }
