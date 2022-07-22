9b550be88a86a41c136b8fd067ac29380207dae7
Brice Ernst <brice.ernst1@gmail.com>
2022-07-22 09:55:02 -0500

    exercise 37

--- src/main/Wall.java
+++ src/main/Wall.java
@@ -1,50 +1,64 @@
 package main;
 
+// EX31
+//public class Wall {
+//    private double width;
+//    private double height;
+//
+//    public Wall() {
+//    }
+//
+//    public Wall(double width, double height) {
+//        if (width < 0){
+//            this.width = 0;
+//        } else {
+//            this.width = width;
+//        }
+//        if (height < 0){
+//            this.height = 0;
+//        } else {
+//            this.height = height;
+//        }
+//    }
+//
+//    public double getWidth() {
+//        return width;
+//    }
+//
+//    public void setWidth(double width) {
+//        if (width < 0){
+//            this.width = 0;
+//        } else {
+//            this.width = width;
+//        }
+//    }
+//
+//    public double getHeight() {
+//        return height;
+//    }
+//
+//    public void setHeight(double height) {
+//        if (height < 0){
+//            this.height = 0;
+//        } else {
+//            this.height = height;
+//        }
+//    }
+//
+//    public double getArea(){
+//        return width * height;
+//    }
+//}
+
+// EX37
 public class Wall {
-    private double width;
-    private double height;
+    private String direction;
 
-    public Wall() {
+    public Wall(String direction) {
+        this.direction = direction;
     }
 
-    public Wall(double width, double height) {
-        if (width < 0){
-            this.width = 0;
-        } else {
-            this.width = width;
-        }
-        if (height < 0){
-            this.height = 0;
-        } else {
-            this.height = height;
-        }
-    }
-
-    public double getWidth() {
-        return width;
-    }
-
-    public void setWidth(double width) {
-        if (width < 0){
-            this.width = 0;
-        } else {
-            this.width = width;
-        }
-    }
-
-    public double getHeight() {
-        return height;
-    }
-
-    public void setHeight(double height) {
-        if (height < 0){
-            this.height = 0;
-        } else {
-            this.height = height;
-        }
-    }
-
-    public double getArea(){
-        return width * height;
+    public String getDirection() {
+        return direction;
     }
 }
